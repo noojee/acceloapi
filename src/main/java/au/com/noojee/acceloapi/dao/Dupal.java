b3fda7c6c9a7b9310738913f9ff68a189de0a0f7
Brett Sutton <bsutton@noojee.com.au>
2018-12-18 14:53:56 +1100

    helper class.

---
+++ src/main/java/au/com/noojee/acceloapi/dao/Dupal.java
@@ -0,0 +1,14 @@
+package au.com.noojee.acceloapi.dao;
+
+public class Dupal<T1, T2>
+{
+	public T1 param1;
+	public T2 param2;
+
+	public Dupal(T1 param1, T2 param2)
+	{
+		this.param1 = param1;
+		this.param2 = param2;
+	}
+
+}
